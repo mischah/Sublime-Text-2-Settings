da6e6caedbceab503cf8fc2c563ef028cb332b3e
Michael Kühnel <mail@michael-kuehnel.de>
2013-09-13 22:11:16 +0200

    Replace HTML-CSS-JS Prettify with Code​Formatter

---
+++ CodeFormatter/codeformatter/formatter.py
@@ -0,0 +1,85 @@
+# @author 		Avtandil Kikabidze
+# @copyright 		Copyright (c) 2008-2013, Avtandil Kikabidze (akalongman@gmail.com)
+# @link 			http://long.ge
+# @license 		GNU General Public License version 2 or later;
+
+import sublime
+import re
+
+try:
+	# Python 3
+	from .phpformatter import PhpFormatter
+	from .jsformatter import JsFormatter
+	from .htmlformatter import HtmlFormatter
+	from .cssformatter import CssFormatter
+	from .pyformatter import PyFormatter
+
+except (ValueError):
+	# Python 2
+	from phpformatter import PhpFormatter
+	from jsformatter import JsFormatter
+	from htmlformatter import HtmlFormatter
+	from cssformatter import CssFormatter
+	from pyformatter import PyFormatter
+
+
+class Formatter:
+	def __init__(self, view=False, file_name=False):
+		self.platform = sublime.platform()
+		self.classmap = {
+			'php': PhpFormatter,
+			'javascript': JsFormatter,
+			'json': JsFormatter,
+			'html': HtmlFormatter,
+			'css': CssFormatter,
+			'python': PyFormatter
+		}
+		self.st_version = 2
+		if sublime.version() == '' or int(sublime.version()) > 3000:
+			self.st_version = 3
+
+		self.syntax_file = view.settings().get('syntax')
+		self.syntax = self.getSyntax()
+		self.file_name = file_name
+		self.settings = sublime.load_settings('CodeFormatter.sublime-settings')
+		self.packages_path = sublime.packages_path()
+
+	def format(self, text):
+
+		try:
+			formatter = self.classmap[self.syntax](self)
+		except Exception as e:
+			stdout = ""
+			stderr = "Formatter for "+self.syntax+" files not supported."
+			return self.clean(stdout), self.clean(stderr)
+
+		try:
+			stdout, stderr = formatter.format(text)
+		except Exception as e:
+			stdout = ""
+			stderr = str(e)
+
+		return self.clean(stdout), self.clean(stderr)
+
+
+	def exists(self):
+		if self.syntax in self.classmap:
+			return True
+		else:
+			return False
+
+	def getSyntax(self):
+		pattern = re.compile(r"Packages/(.+?)/.+?\.tmLanguage")
+		m = pattern.search(self.syntax_file)
+		found = ""
+		if (m):
+			for s in m.groups():
+				found = s
+				break
+		return found.lower()
+
+	def clean(self, string):
+		if hasattr(string, 'decode'):
+			string = string.decode('UTF-8')
+
+		return re.sub(r'\r\n|\r', '\n', string)
